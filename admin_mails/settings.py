52c26eb9d1ae3767bc763a76f308c4ac74742da9
Luis Vargas <luisvargasr10@gmail.com>
2021-02-23 20:14:06 -0600

    env var

--- admin_mails/settings.py
+++ admin_mails/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+g!%-ze5ohkb43vfxk(zi!5kp5%vflytlt-+7+f%7t$^(y_qli'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
